02e887b879194f3b242f0bbd3497da97280eafcb
Jiin <jiinkim02@gmail.com>
2021-05-08 15:16:23 -0700

    add party page

--- challenge-2/hot-and-rdi-to-go/src/App.js
+++ challenge-2/hot-and-rdi-to-go/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import PartyPage from "./pages/PartyPage.js";
 import QuestionPage from "./pages/QuestionPage.js";
 import ResultPage from "./pages/ResultPage.js";
 import cheese from "./img/cheeseq.jpeg";
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="App-header">HOT AND %RDI</h1>
-      <QuestionPage />
+      <PartyPage />
     </div>
   );
 }
