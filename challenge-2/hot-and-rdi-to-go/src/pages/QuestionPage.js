836117e62d4bf264c796ef7f3a34da7a9dda0ed2
Matthew Nieva <matthewcn56@gmail.com>
2021-05-08 15:37:54 -0700

    Changed Some Values

--- challenge-2/hot-and-rdi-to-go/src/pages/QuestionPage.js
+++ challenge-2/hot-and-rdi-to-go/src/pages/QuestionPage.js
@@ -3,9 +3,13 @@ import Question from "../components/Question.js";
 import cheese from "../img/cheeseq.jpeg";
 import "./QuestionPage.css";
 import { ScoreContext } from "../components/ScoreContext";
+import { ScoreProvider } from "../components/ScoreContext";
 
 export default function QuestionPage(props) {
   const { questionList } = useContext(ScoreContext);
+  const questionComponents = questionList.map((item) => (
+    <Question question={item.question} response={item.response} />
+  ));
   return (
     <div className="question-page">
       <h2 className="progress">1/10</h2>
