b69d09261bdd069306042862a7e7063d6b78f6b7
edmondywen <edmondywen@gmail.com>
2021-05-08 09:59:54 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rax-of-ribs
+//
+//  Created by Edmond Wen on 5/8/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
